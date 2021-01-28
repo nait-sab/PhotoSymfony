7007a64ad3ce5475fb999e42d37ceeab0972f424
nait-sab <naitsab.boulogne@gmail.com>
2021-01-28 16:35:20 +0100

    fix home page for show only client files

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\UploadRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,11 +20,41 @@ class HomeController extends AbstractController
         ]);
     }
 
-    public function chercherPhotos(): Response
+    public function chercherPhotos(UserRepository $clients, UploadRepository $images): Response
     {
+        // 1 - Récupérer l'id du propriétaire de l'image
+        $userName = $this->getUser()->getUsername();
+        $userPassword = $this->getUser()->getPassword();
+        $clientID = $clients->findOneBy(['username' => $userName, 'password' => $userPassword])->getId();
+
+        // 2 - Récupérer le dossier des images
         $repo = scandir(__DIR__ . "/../../public/uploads");
 
-        return $this->render("home/voirImages.html.twig", ["liste" => $repo]);
+        // 3 - Récupérer la liste des images du client
+        $repoClient = $images->findBy(['proprietaire' => $clientID]);
+
+        // 4 - Récupérer la liste des noms
+        $listeImages = [];
+        $slot = 0;
+        foreach ($repoClient as $image)
+        {
+            $listeImages[$slot] = $image->getName();
+            $slot++;
+        }
+
+        // 5 - Extraire uniquement les images apartenant au client
+        $listeVisible = [];
+        $slot = 0;
+        foreach ($repo as $image)
+        {
+            if (in_array($image, $listeImages))
+            {
+                $listeVisible[$slot] = $image;
+                $slot++;
+            }
+        }
+
+        return $this->render("home/voirImages.html.twig", ["liste" => $listeVisible]);
     }
 
     /**
