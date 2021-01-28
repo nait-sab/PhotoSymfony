650be2dc0382e654b36562c9864d2b78d4352a6b
nait-sab <naitsab.boulogne@gmail.com>
2021-01-28 17:23:40 +0100

    add import file type validation for only jpeg and png

--- src/Form/UploadType.php
+++ src/Form/UploadType.php
@@ -8,13 +8,27 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\File;
 
 class UploadType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', FileType::class, ['label' => 'Choississez votre image'])
+            ->add('name', FileType::class, [
+                'label' => 'Choississez votre image',
+                'attr' => ['accept' => "image/jpeg, image/png"],
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png'
+                        ],
+                        'mimeTypesMessage' => 'Format accepté : JPEG et PNG. Taille maximum : 5mo'
+                    ])
+                ]
+            ])
             ->add('submit', SubmitType::class);
         ;
     }
