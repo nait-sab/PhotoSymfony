feacbb9d91645e2673405ea02cf645c0cd7533c1
nait-sab <naitsab.boulogne@gmail.com>
2021-01-30 18:47:16 +0100

    add delete function for all image (delete from folder + database)

--- src/Controller/DisplayController.php
+++ src/Controller/DisplayController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Repository\UploadRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityNotFoundException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,7 +28,27 @@ class DisplayController extends AbstractController
             $proprietaireID = $image->getProprietaire();
             $user = $users->findOneBy(['id' => $proprietaireID])->getUsername();
 
-            return $this->render('display/view.html.twig', ["nom" => $nom, "auteur" => $user]);
+            return $this->render('display/view.html.twig', ["nom" => $nom, "auteur" => $user, "id" => $id]);
         }
+    }
+
+    /**
+     * @Route("/display/remove/{id}", name="delete_image")
+     */
+    public function removeImage(int $id, UploadRepository $images, EntityManagerInterface $manager)
+    {
+        // Récupérer l'entité
+        $image = $images->findOneBy(['id' => $id]);
+
+        // Retirer l'entité du dossier du site
+        $nom = $image->getName();
+        unlink(__DIR__ . "/../../public/uploads/" . $nom);
+
+        // Retirer l'entité du database
+        $manager->remove($image);
+        $manager->flush();
+
+        // Ramener le client au menu
+        return $this->render('home/index.html.twig');
     }
 }
