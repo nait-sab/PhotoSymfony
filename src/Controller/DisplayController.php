42c3f0171f520ad03bb401a3269857a396629fd2
nait-sab <naitsab.boulogne@gmail.com>
2021-01-31 16:54:52 +0100

    deploy the visibility system on all image

--- src/Controller/DisplayController.php
+++ src/Controller/DisplayController.php
@@ -24,24 +24,36 @@ class DisplayController extends AbstractController
             return $this->render('display/error.html.twig');
         else
         {
+            // Récupérer les infos de l'image
             $nom = $image->getName();
             $proprietaireID = $image->getProprietaire();
             $auteur = $users->findOneBy(['id' => $proprietaireID])->getUsername();
 
+            // Récupérer l'utilisateur actuel
             $userName = $this->getUser()->getUsername();
             $userPassword = $this->getUser()->getPassword();
             $clientID = $users->findOneBy(['username' => $userName, 'password' => $userPassword])->getId();
 
+            // Vérifier si l'utilisateur est l'auteur pour afficher le bouton Supprimer l'image
             $proprietaire = false;
             if ($proprietaireID == $clientID)
                 $proprietaire = true;
 
-            return $this->render('display/view.html.twig', [
-                "nom" => $nom,
-                "auteur" => $auteur,
-                "id" => $id,
-                "showDelete" => $proprietaire
-            ]);
+            // Vérifier si l'image est publique pour l'afficher ou non aux autres utilisateurs
+            // La rendre visible tout de même si l'auteur est l'utilisateur actuel
+            $public = $image->getPublic();
+            if ($proprietaire)
+                $public = true;
+
+            if ($public)
+                return $this->render('display/view.html.twig', [
+                    "nom" => $nom,
+                    "auteur" => $auteur,
+                    "id" => $id,
+                    "showDelete" => $proprietaire
+                ]);
+            else
+                return $this->render('display/private.html.twig');
         }
     }
 
